3bed5b5063945f7ea8add2a32882ff6f38f12610
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2026-01-15 15:43:58 +0530

    refactor:

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,3 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
-use actix_web::dev::Server;
-use std::net::TcpListener;
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-#[derive(serde::Deserialize)]
-pub struct FormData {
-    pub email: String,
-    pub name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok()
-}
-
-pub fn run(listner: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listner)?
-    .run();
-    Ok(server)
-}
+pub mod routes;
+pub mod configuration;
+pub mod startup;
