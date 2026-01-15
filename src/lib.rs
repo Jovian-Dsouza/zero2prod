2e58bb4dcb0fd3b2f6b58fa260a06ac2a30cd00e
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2026-01-15 14:35:04 +0530

    feat: working tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,28 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
-
+use actix_web::dev::Server;
+use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    pub email: String,
+    pub name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub fn run(listner: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .listen(listner)?
+    .run();
+    Ok(server)
 }
