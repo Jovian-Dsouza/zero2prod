f09850cf21371187f9657872d8ae5c78025908ad
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2026-01-19 16:12:24 +0530

    feat: add basic logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,7 +13,9 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> impl Responder {
-    sqlx::query!(
+    log::info!("Adding '{}' '{}' as new subscriber", form.email, form.name);
+    log::info!("Saving new subscriber details in the database");
+    match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -25,6 +27,14 @@ pub async fn subscribe(
     )
     .execute(pool.get_ref())
     .await
-    .expect("Failed to execute query.");
-    HttpResponse::Ok()
+    {
+        Ok(_) => {
+            log::info!("New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        },
+        Err(e) => {
+            log::error!("Failed to execute query: {:?}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
